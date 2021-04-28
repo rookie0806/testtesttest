fdb65f9461eef62f6d047abc226e6b696248e6b5
unknown <jbshs2013@naver.com>
2021-04-28 15:59:11 +0900

    first commit

--- rsialarm.py
+++ rsialarm.py
@@ -39,6 +39,96 @@ number = []
 repeat = []
 pyungdan = []
 rsi_lasts = []
+
+def buy(coin,volume,price):
+    query = {
+    'market': 'KRW-'+coin,
+    'side': 'bid',
+    'price': price,
+    'volume': '',
+    'ord_type': 'price',
+    }
+    query_string = urlencode(query).encode()
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+
+    res = requests.post(server_url + "/v1/orders", params=query, headers=headers)
+    
+    if(res.status_code==201):
+        return True
+    else:
+        return False
+
+def sell(coin,volume,price):
+    query = {
+    'market': 'KRW-'+coin,
+    'side': 'ask',
+    'price': '',
+    'volume': volume,
+    'ord_type': 'market',
+    }
+    query_string = urlencode(query).encode()
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+
+    res = requests.post(server_url + "/v1/orders", params=query, headers=headers)
+    if(res.status_code==201):
+        #return float(res.json()["price"])
+        return 1
+    else:
+        return 0
+
+def get_my_value(coin):
+    query = {
+        'market': 'KRW-'+coin,
+    }
+    query_string = urlencode(query).encode()
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+    res = requests.get(server_url + "/v1/orders/chance", params=query, headers=headers)
+    balance = float(res.json()["ask_account"]["balance"])
+    avg_buy_price = float(res.json()["ask_account"]["avg_buy_price"])
+    return balance,avg_buy_price
+
 for i in range(0,len(symbols)):
     buycoin.append(0)
     number.append(0)
@@ -50,7 +140,7 @@ while True:
     for i in range(0,len(symbols)):
         try:
             time.sleep(0.5)
-            url = "https://api.upbit.com/v1/candles/minutes/5"
+            url = "https://api.upbit.com/v1/candles/minutes/1"
             querystring = {"market":"KRW-"+symbols[i],"count":"500"}
             response = requests.request("GET", url, params=querystring)
             data = response.json()
@@ -70,23 +160,37 @@ while True:
 
             rsi_now = rsi(df, 14).iloc[-1]
             rsi_last = rsi(df, 14).iloc[-2]
+            rsi_last2 = rsi(df, 14).iloc[-3]
             price = df["trade_price"].iloc[-1]
             if(rsi_lasts[i]!=rsi_last):
-                if(rsi_now>=35 and rsi_last<35):
+                if(rsi_now>=35 and rsi_now>=rsi_last2 and rsi_last<35):
                     if(repeat[i]==1):
-                        now_buy = 50000/price
+                        now_buy = 30000/price
                     else:
-                        now_buy = 50000/price * (1 + 30*(pyungdan[i]-price)/(pyungdan[i]+price)/2)
+                        now_buy = 30000/price * (1 + 30*(pyungdan[i]-price)/(pyungdan[i]+price)/2)
                     number[i] = number[i] + now_buy
                     buycoin[i] = buycoin[i] + price  * now_buy
                     pyungdan[i] = buycoin[i]/number[i]
                     bot.sendMessage(chat_id = '1780594186', text="["+symbols[i]+"] 구매("+str(repeat[i])+") 신호 "+str(price )+"원, 총 구매 :"+str(price* now_buy))
+                    buy(symbols[i],price* now_buy,price* now_buy)
                     repeat[i] = repeat[i] + 1
-                if(rsi_now>=65 and number[i]!=0):
-                    bot.sendMessage(chat_id = '1780594186', text="["+symbols[i]+"] 판매 신호(65) "+str(price)+"원")
+                if(rsi_now>=70 and number[i]!=0):
+                    bot.sendMessage(chat_id = '1780594186', text="["+symbols[i]+"] 판매 "+str(price)+"원")
+                    mybal,my_avg_price = get_my_value(symbols[i])
+                    if(mybal!=0.0):
+                        sell(symbols[i],mybal,mybal)
                     number[i] = 0
                     buycoin[i] = 0
                     repeat[i] = 1
+                    
+                if(rsi_last>=55 and rsi_now<=rsi_last2  and number[i]!=0):
+                    bot.sendMessage(chat_id = '1780594186', text="["+symbols[i]+"] 판매"+str(price)+"원")
+                    number[i] = 0
+                    buycoin[i] = 0
+                    repeat[i] = 1
+                    mybal,my_avg_price = get_my_value(symbols[i])
+                    if(mybal!=0.0):
+                        sell(symbols[i],mybal,mybal)
                 rsi_lasts[i] = rsi_last
         except Exception as e:
             print(e)
